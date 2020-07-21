8804d49f3d26dbfad7ba6df21ebf7475d91986f6
Leviosah <conormag60@gmail.com>
2020-07-20 22:56:23 -0700

    first commit

---
+++ forecast.js
@@ -0,0 +1,24 @@
+
+var paramProp = {   
+    'appid': '0b25d9f963f5f0b43ea328aeb3d40e40',    
+}
+     
+function generateQuery(city) 
+{
+    return `http://api.openweathermap.org/data/2.5/weather?appid=`+paramProp.appid+"&q="+city;
+}
+        
+
+$("#search-btn").click(function() {
+    var city=$("#city-input").val();
+    $.ajax({
+        url: generateQuery(city),
+        method: "GET"
+    
+    }).then(function(data) {
+        $("#location").html(data["name"]);
+        $("#cel").html("°C"+Math.round(data["main"]["temp"] - 273.15));
+        $("#far").html("°F"+Math.round((data["main"]["temp"] - 273.15)*9/5+32));
+    $("#curr-icon").attr("src","http://openweathermap.org/img/wn/"+data["weather"][0]["icon"]+"@2x.png");
+    });
+});
